826bf7255cdf9a475dea967409a8f81726687f87
warker <warker@mini.(none)>
2010-12-17 23:39:47 +0100

    initial upload

---
+++ ledwandutil.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+from socket import *
+from array import array
+import math
+import urllib
+
+''' Utility module for LED-Screen at CCCB
+written by Warker, Ron, Denis
+'''
+
+def HttpRequest(url, params=dict()):
+    if(params != dict()):
+        f = urllib.urlopen("%s?%s" % (url, urllib.urlencode(params)))
+        print "requesting: %s?%s" % (url, urllib.urlencode(params))
+    else:
+        f = urllib.urlopen(url)
+    return f.read()
+
+class Ledwand:
+
+    def __init__(self, host="172.23.42.120", port=2342, linelen=56, lines=19):
+        self.UdpSocket = socket(AF_INET, SOCK_DGRAM)
+        self.UdpAddress = (host, port)
+        self.Linelen = linelen
+        self.Lines = lines
+        self.clearBuf()
+
+    def processline(self, line):
+        line = array('c', str(line))
+        tempbuf = array('c', " "*self.Linelen)
+        if len(line) < self.Linelen:
+            tempbuf[0:len(line)] = line
+        else:
+            tempbuf = line[0:self.Linelen]
+        return tempbuf
+
+    def sendline(self, linenum, line):
+        self.setlineraw(0, linenum, self.processline(line).tostring())
+
+    def convert(self, x):
+        x1 = math.floor(x / 256)
+        x2 = math.fmod(x, 256)
+        return chr(int(x2)) + chr(int(x1))
+
+    def request(self, cmd, xpos, ypos, xs, ys, text):
+        cmd = self.convert(cmd)
+        xpos = self.convert(xpos)
+        ypos = self.convert(ypos)
+        xs = self.convert(xs)
+        ys = self.convert(ys)
+        self.UdpSocket.settimeout(1)
+        self.UdpSocket.sendto(cmd + xpos + ypos + xs + ys + text, self.UdpAddress)
+        try:
+            self.UdpSocket.recv(4096)
+        except timeout:
+            print "Warning: last transmition timed out"
+
+    def setline(self, x, y, data):
+        self.request(4, x, y, 1, 1, data)
+
+    def setlineraw(self, x, y, data):
+        data = data.decode("utf8").encode("cp437")
+        #print "sending:", 3, x, y, len(data), 20, data.decode("cp437").encode("utf8")
+        self.request(3, x, y, len(data), 1, data) # send raw
+
+    def clear(self):
+        self.request(2, 0, 0, 0, 0, '') # clear
+
+    def softreset(self):
+        self.request(8, 0, 0, 0, 0, '')
+
+    def hardreset(self):
+        self.request(11, 0, 0, 0, 0, '')
+
+    def redraw(self):
+        self.request(8, 0, 0, 0, 0, ' ') # redraw
+
+    def setbrightness(self, val):
+        self.request(7, 0, 0, 0, 0, chr(val)) # brightness
+
+    def closeSocket(self):
+        self.UdpSocket.close()
+
+    def changeLineInBuf(self, linenum, line):
+        self.DisplayBuf[linenum*self.Linelen:(linenum+1)*self.Linelen] = self.processline(line)
+
+    def sendfilledBuf(self):
+        self.request(3, 0, 0, self.Linelen, self.Lines, self.DisplayBuf.tostring().decode("utf8").encode("cp437"))
+    
+    def clearBuf(self):
+        self.DisplayBuf = array('c', " "*self.Linelen*self.Lines)
+        
+
+class LedwandProvider:
+
+    def __init__(self, ledwand = Ledwand()):
+        self.Ledwand = ledwand
+
+    def getData(self):
+        pass
+
+    def displayData(self):
+        data = self.getData()
+        linecount = 0
+        for obj in data:
+            print "data:", obj
+            self.Ledwand.sendline(linecount, obj)
+            linecount += 1
---
+++ vbbregex.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+import re
+import time
+from ledwandutil import *
+
+'''
+VBB requester for LED-Screen at CCCB
+written by warker
+'''
+
+
+HTMLCODES = [
+        ["&#246;", "ö"],
+        ["&#252;", "ü"],
+        ["&#228;", "ä"],
+        ["&#214;", "Ö"],
+        ["&#220;", "Ü"],
+        ["&#196;", "Ä"],
+        ["&#223;", "ß"]
+    ]
+
+def subHtmlcode(s, code=HTMLCODES):
+    for c in code:
+        s = s.replace(c[0], c[1].decode("utf8").encode("iso-8859-1"))
+    return s
+
+class VbbFahrt:
+    def __init__(self, name, time, timeis, dest, platform, diff = 0):
+        self.Name, self.Time, self.Timeis, self.Dest, self.Platform, self.Diff = name, time, timeis, dest, platform, diff 
+
+    def __repr__(self):
+        if len(self.Timeis) <= 0:
+            self.Timeis = "        "
+        if len(self.Platform) > 3: #some vbb request are just wrong
+            self.Platform = " "
+        if len(self.Platform) > 0:
+            #res = "%2s %4s %5s %8s %s" %(self.Platform, self.Name, self.Time, self.Timeis, self.Dest)
+            res = "%2s %4s %8s %s" %(self.Platform, self.Name, `self.Diff`+"min", self.Dest)
+        else:
+            #res = "   %4s %5s %8s %s" %(self.Name, self.Time, self.Timeis, self.Dest)
+            res = "   %4s %8s %s" %(self.Name, `self.Diff`+"min", self.Dest)
+        return res.replace("&nbsp;", "");
+
+class VbbRequest(LedwandProvider):
+    def __init__(self, url = "http://www.vbb-fahrinfo.de/hafas/stboard.exe/dn?", stop="S+U Friedrichstr. Bhf (Berlin)", howmany=10):
+        LedwandProvider.__init__(self)
+        self.Url = url 
+        self.Stop = stop
+        self.Howmany = howmany
+        self.List = list()
+
+    def getHtml(self):
+        return HttpRequest(self.Url, {"boardOverview":"yes", "maxJourneys":self.Howmany, "boardType":"dep", "input":self.Stop, "start":"Start"})        
+
+    def getData(self):
+        self.List.append("Haltestelle "+ self.Stop + " " + time.strftime("%H:%M"))
+        self.List.append("%2s %4s %8s %s" %("Gl", "Typ", "Ankunft", "Richtung"))
+        data = subHtmlcode(re.sub(r"[\t\r\n]","", self.getHtml().replace("&nbsp;",""))).decode("iso-8859-1").encode("utf8")
+        m = re.findall(r"<tr class=\"depboard-\w+\".*?>.*?</tr>", data)
+        for match in m:
+            m1 = re.search(r"<td class=\"time\">(?P<time>.*?)</td>(<td class=\"prognosis(.*?\"><img.*?/>|.*?\">)(ca.)?(?P<timeis>.*?)</td>)?<td class=\"product\">.*?/> *(?P<name>[A-Z0-9]*)</a></td><td class=\"timetable\"><strong>(?P<to>.*?)</strong></td>(<td class=\"platform\">(?P<platform>.+?)( *<br.*?)?</td>)?", match)
+            if m1 != None:
+                obj = VbbFahrt(m1.group("name"), m1.group("time"), m1.group("timeis"), m1.group("to"), m1.group("platform"))
+                if obj.Timeis is None: # Sometimes there is no predicted time, so we repair
+                    obj.Timeis = ""
+                if obj.Platform is None:
+                    obj.Platform = ""
+
+                if obj.Timeis != "&nbsp;" and len(obj.Timeis) > 0: #debug
+                    obj.Diff = self.timediffinmin(obj.Timeis) # debug
+                else:
+                    obj.Diff = self.timediffinmin(obj.Time)
+                self.List.append(obj)
+            else:
+                print "MISSED:", match
+        return self.List
+
+    def clear(self):
+        self.List = list()
+
+    def timediffinmin(self, timestr):
+        t = time.localtime()
+        d = time.strptime(timestr, "%H:%M")
+        diff = (d.tm_hour - t.tm_hour)*60 + (d.tm_min - t.tm_min)
+        return diff
+
+if __name__ == "__main__":
+    vbb = VbbRequest(howmany=17)
+    vbb.Ledwand.setbrightness(4)
+    vbb.Ledwand.clear()
+    while(1):
+        vbb.clear()
+        vbb.displayData()
+        time.sleep(30)
